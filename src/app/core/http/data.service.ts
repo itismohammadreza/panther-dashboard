125cf954e2024b9d4bb16ed4fbe9f9d28ab4e63b
mohammadreza99 <shahbaziweb@gmail.com>
2023-06-15 17:45:46 +0330

    update service base on models

--- src/app/core/http/data.service.ts
+++ src/app/core/http/data.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {lastValueFrom} from "rxjs";
 import {ApiService} from '@core/http';
+import {DataModel, ModelItem} from "@core/models/data.models";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,14 @@ export class DataService extends ApiService {
   }
 
   getModels() {
-    const res = this._get<any>('assets/db.json');
-    return lastValueFrom(res);
+    const db = this._get<any>('assets/db.json');
+    const res = db.pipe(map(x => x.models));
+    return lastValueFrom<DataModel[]>(res);
+  }
+
+  getItems(model: string) {
+    const db = this._get<any>('assets/db.json');
+    const res = db.pipe(map(x => x.items[model]));
+    return lastValueFrom<ModelItem[]>(res);
   }
 }
