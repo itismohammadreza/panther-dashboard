7d28513c6be19e8ff4f8ef2df5682146f2d14efb
mohammadreza99 <shahbaziweb@gmail.com>
2023-06-15 17:45:31 +0330

    create data models

---
+++ src/app/core/models/data.models.ts
@@ -0,0 +1,13 @@
+export interface DataModel {
+  name: string;
+  path: string;
+  app: string;
+  index: string;
+}
+
+export interface ModelItem {
+  id: number;
+  first_name: string;
+  last_name: string;
+  age: number;
+}
