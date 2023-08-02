529e0b609e16928900736813e6596e12860ee0f5
mohammadreza99 <shahbaziweb@gmail.com>
2023-08-02 17:28:51 +0330

    change api address

--- src/app/core/http/data.service.ts
+++ src/app/core/http/data.service.ts
@@ -1,28 +1,36 @@
 import {Injectable} from '@angular/core';
 import {lastValueFrom} from "rxjs";
 import {ApiService} from '@core/http';
-import {Database, DatabaseItem, TestModelItem} from "@core/models/data.models";
+import {Database, DatabaseRecord, DataModel} from "@core/models/data.models";
 import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService extends ApiService {
-  private endpoint: string = '';
+  private endpoint: string = '_panel';
 
   constructor() {
     super();
   }
 
   getModels() {
-    const db = this._get<Database>(this.endpoint);
-    const res = db.pipe(map(x => x.models));
+    const res = this._get<DataModel[]>(`${this.endpoint}`);
     return lastValueFrom(res);
   }
 
-  getItems(model: string) {
-    const db = this._get<Database>(this.endpoint);
-    const res = db.pipe(map(x => x.items[model] as DatabaseItem<TestModelItem>));
+  getRecords(index: string) {
+    const res = this._get<DatabaseRecord<any>>(`${this.endpoint}/${index}`);
+    return lastValueFrom(res);
+  }
+
+  getRecordById(index: number, id: string) {
+    const res = this._get<any>(`${this.endpoint}/${index}/${id}`);
+    return lastValueFrom(res);
+  }
+
+  deleteRecord(index: number, id: string) {
+    const res = this._delete<any>(`${this.endpoint}/${index}/${id}`);
     return lastValueFrom(res);
   }
 }
